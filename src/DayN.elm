7cc39899f7281d0b2d0bb3bf7559710cb401efb9
Glyn Owen Hanmer <glynhanmer@gmail.com>
2020-12-05 09:18:32 +0000

    Add day template

---
+++ src/DayN.elm
@@ -0,0 +1,39 @@
+module Day5 exposing (main)
+
+import AdventOfCode
+import Browser
+import Dict
+
+
+main =
+    AdventOfCode.day
+        { input = inputReal
+        , testInput = inputTest
+        , part1 = part1
+        , part1TestExpected = Nothing
+        , part1Expected = Nothing
+        , part2 = part2
+        , part2Expected = Nothing
+        , debugWindows = \_ -> []
+        }
+
+
+part1 : List String -> String
+part1 input =
+    ""
+
+
+part2 : List String -> String
+part2 input =
+    ""
+
+
+inputTest : List String
+inputTest =
+    String.split "\n"
+        """"""
+
+
+inputReal : List String
+inputReal =
+    String.split "\n" """"""
